4b7daea6a80491162187b01f3d315b623b80f5f7
tdanielsen <tdanielsen@wisc.edu>
2014-11-15 12:39:21 -0600

    Implemented GuessingGame with user input
    All that is left if input from a file.

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -28,15 +28,28 @@ public class GuessingGame
 	                { 
 	
 	                case 'o':
-	                	if (questionTree.getCurrent() == null)
+	                	try
 	                	{
-	                		throw new IllegalBinaryTreeOpException("Empty Tree");
+	                		questionTree.getCurrent();
+	                		questionTree.print();
 	                	}
-	                	questionTree.print();
+	                	catch (IllegalBinaryTreeOpException e)
+	                	{
+	                		System.out.println("Empty Tree");
+	                	}
+	                	
 	                    break;
 	
 	                case 'p':
-	                    // TODO *** implement this option ***
+	                	try
+	                	{
+		            		questionTree.getCurrent();
+		            		play(scanner, questionTree);
+	                	}
+	                	catch (IllegalBinaryTreeOpException e)
+	                	{
+	                		questionTree = restart(scanner, questionTree);
+	                	}
 	                    break;
 	
 	                case 'q':
@@ -44,15 +57,7 @@ public class GuessingGame
 	                    break;
 	
 	                case 'r':
-	                	System.out.println("Please enter a question.");
-	                	input = scanner.nextLine();
-	                	questionTree = new BinaryTree<String>(input);
-	                	System.out.println("Please enter something that is true for that question.");
-	                	input = scanner.nextLine();
-	                	questionTree.addLeftChild(input);
-	                	System.out.println("Please enter something that is false for that question.");
-	                	input = scanner.nextLine();
-	                	questionTree.addRightChild(input);
+	                	questionTree = restart(scanner, questionTree);
 	                    break;
 	
 	                default: //ignore invalid commands
@@ -63,4 +68,65 @@ public class GuessingGame
             } 
 		}
 	} 
+	private static BinaryTree<String> restart(Scanner scanner, BinaryTree<String> tree) throws IllegalBinaryTreeOpException
+	{
+		String input = "";
+		System.out.println("Please enter a question.");
+    	input = scanner.nextLine();
+    	tree = new BinaryTree<String>(input);
+    	System.out.println("Please enter something that is true for that question.");
+    	input = scanner.nextLine();
+    	tree.addLeftChild(input);
+    	System.out.println("Please enter something that is false for that question.");
+    	input = scanner.nextLine();
+    	tree.addRightChild(input);
+    	return tree;
+	}
+	private static void play(Scanner scanner, BinaryTree<String> tree) throws IllegalBinaryTreeOpException
+	{
+		String input = "";
+		boolean guessingInProgress = true;
+		while (guessingInProgress)
+		{
+			if (tree.isLeaf())
+			{
+				System.out.println("I guess: " + tree.getCurrent() + ". Was I right?");
+				input = scanner.nextLine();
+				if (input.equalsIgnoreCase("y"))
+				{
+					System.out.println("I win.");
+				}
+				if (input.equalsIgnoreCase("n"))
+				{
+					failedGuess(scanner, tree);
+				}
+				guessingInProgress = false;
+			}
+			else
+			{
+				System.out.println(tree.getCurrent());
+				input = scanner.nextLine();
+				if (input.equalsIgnoreCase("y"))
+				{
+					tree.goLeft();
+				}
+				if (input.equalsIgnoreCase("n"))
+				{
+					tree.goRight();
+				}
+			}
+		}
+	}
+	private static void failedGuess(Scanner scanner, BinaryTree<String> tree) throws IllegalBinaryTreeOpException
+	{
+		String input = "";
+		String oldAnswer = tree.getCurrent();
+		System.out.println("Darn. Ok, tell me a question that is true for your answer, but false for my guess.");
+		input = scanner.nextLine();
+		tree.changeCurrent(input);
+		System.out.println("Thanks. And what were you thinking of?");
+		input = scanner.nextLine();
+		tree.addLeftChild(input);
+		tree.addRightChild(oldAnswer);
+	}
 }
